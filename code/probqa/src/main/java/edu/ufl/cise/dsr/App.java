171e7c1f4659e75d9df9928dc9238abbdf39ccf1
cegme <christangrant@gmail.com>
2015-03-24 16:44:40 -0400

    Slight architecture changes

--- code/probqa/src/main/java/edu/ufl/cise/dsr/App.java
+++ code/probqa/src/main/java/edu/ufl/cise/dsr/App.java
@@ -18,13 +18,26 @@ import java.util.List;
 public class App {
 
   public static void tripleExtractor() throws MalformedQueryException {
-    final String queryString =
+    /*final String queryString =
       "PREFIX  dc:  <http://purl.org/dc/elements/1.1/>"+
       "PREFIX  ns:  <http://example.org/ns#>"+
       "SELECT  ?title ?price"+
       "WHERE   { ?x ns:price ?price ."+
       "FILTER (?price < 30.5)"+
-      "?x dc:title ?title . }";
+      "?x dc:title ?title . }";*/
+    final String queryString = 
+      "PREFIX fb: <http://rdf.freebase.com/ns/>\n"+
+      "SELECT DISTINCT ?x2 ?x2name\n"+
+      "WHERE {\n"+
+      "\tfb:en.justin_bieber fb:people.person.sibling_s ?x1 .\n"+
+      "\t?x1 fb:people.sibling_relationship.sibling ?x2 .\n"+
+      "\n"+
+      "\tFILTER (?x2 != fb:en.justin_bieber) .\n"+
+      "\tOPTIONAL { ?x2 fb:type.object.name ?x2name }\n"+
+      "}\n"+
+      "LIMIT 10";
+
+    System.out.println(queryString);
 
     SPARQLParser parser = new SPARQLParser();
     ParsedQuery query = parser.parseQuery(queryString, null); // maybe add freebase
